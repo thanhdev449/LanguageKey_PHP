a6b685bbc233296e22f739f69395dfc86c3e7604
thanhdev449 <thanh.nguyen@vinasaver.vn>
2020-07-28 16:10:57 +0700

    update code

--- front-end-react/src/App.js
+++ front-end-react/src/App.js
@@ -1,28 +1,62 @@
 import React from "react";
 //import logo from "./logo.svg";
 import "./App.css";
-import { Button } from "reactstrap";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+//import { Button } from "reactstrap";
+
+function Home() {
+    return <h2>Home111111</h2>;
+}
+
+function About() {
+    return <h2>About</h2>;
+}
+
+function Users() {
+    return <h2>Users</h2>;
+}
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                {/* <img src={logo} className="App-logo" alt="logo" />
+        <Router>
+            <div className="App">
+                <header className="App-header">
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/about">About</Link>
+                            </li>
+                            <li>
+                                <Link to="/users">Users</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <Switch>
+                        <Route path="/about">
+                            <About />
+                        </Route>
+                        <Route path="/users">
+                            <Users />
+                        </Route>
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                    {/* <img src={logo} className="App-logo" alt="logo" />
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
                 </a> */}
-                <Button color="primary">primary</Button>
-                <Button color="secondary">secondary</Button>
-                <Button color="success">success</Button>
-                <Button color="info">info</Button>
-                <Button color="warning">warning</Button>
-                <Button color="danger">danger</Button>
-                <Button color="link">link</Button>
-            </header>
-        </div>
+                    {/* <Button color="primary">primary</Button>
+                <Button color="link">link</Button> */}
+                </header>
+            </div>
+        </Router>
     );
 }
 
